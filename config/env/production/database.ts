770e7fbf1d08219d5f6498578341dfabf0a64244
stalugaexpert <stalegapawel@gmail.com>
2022-11-14 22:14:42 +0100

    Added postgres.

---
+++ config/env/production/database.ts
@@ -0,0 +1,16 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'strapi'),
+      user: env('PGUSER', 'strapi'),
+      password: env('PGPASSWORD', 'strapi'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+    },
+    debug: false,
+  },
+});
